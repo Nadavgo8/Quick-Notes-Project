08a76b3e74250b31eaf5019de967f566a90af506
Nadavgo8 <19nadav99@gmail.com>
2025-08-13 18:34:38 +0300

    added step 2 - date part

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,10 +1,14 @@
+import { dayjs } from "./lib/dayjs";
+
 export default function Note({ note }) {
-  const date = new Date(note.createdAt).toLocaleString();
+  const formattedDate = dayjs(note.createdAt).format("MMM Do h:mm A");
+  // "MMM" = Aug, "Do" = 31st, "h:mm A" = 12:30 PM
+
   return (
     <article className="note-card">
       <p className="note-text">{note.text}</p>
       <time className="note-date" dateTime={note.createdAt}>
-        {date}
+        {formattedDate}
       </time>
     </article>
   );
